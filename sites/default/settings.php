c881c2d75be37d64b35be2249703b6611e668819
jadams0128 <jadams0128@hotmail.com>
2018-09-08 00:15:36 -0400

    added the redirect for https in settings php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -28,7 +28,7 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
   // Redirect to https://$primary_domain in the Live environment
   if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
     /** Replace www.example.com with your registered domain name */
-    $primary_domain = 'www.example.com';
+    $primary_domain = 'www.adamstechky.com';
   }
   else {
     // Redirect to HTTPS on every Pantheon environment.
